69ab71afd0bfa2bf9a61f5db2a17681373565595
YTBJero <nguyenhieucoder47@gmail.com>
2022-05-24 17:27:00 +0700

    1.0.2 is next

--- src/davidglitch04/iLand/VersionInfo.php
+++ src/davidglitch04/iLand/VersionInfo.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace davidglitch04\iLand;
 
 final class VersionInfo {
-	public const PLUGIN_VERSION = '1.0.1';
-	public const IS_DEVELOPMENT_BUILD = false;
+	public const PLUGIN_VERSION = '1.0.2';
+	public const IS_DEVELOPMENT_BUILD = true;
 	public const BUILD_CHANNEL = 'stable';
 	public const CONFIG_VERSION = 281;
 }
